35c56bdee6d8d621208eb3958edcd7c6571481f5
heyfey <tsungtsohsieh@gmail.com>
2021-04-13 02:27:18 +0800

    Initial commit

---
+++ examples/py/callback.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from tensorflow import keras
+
+def MyCallback(keras.callbacks.Callback):
+    def on_train_begin(self, logs=None):
+        pass
+
+    def on_train_end(self, logs=None):
+        pass
+
+    def on_epoch_begin(self, epoch, logs=None):
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        pass
+
+    def on_train_batch_begin(self, batch, logs=None):
+        pass
+
+    def on_train_batch_end(self, batch, logs=None):
+        pass
+
