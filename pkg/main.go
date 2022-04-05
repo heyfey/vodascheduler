bbcc57abb3ff880c794010e7a3132cb3cfdb61bd
heyfey <tsungtsohsieh@gmail.com>
2022-04-05 16:36:25 +0800

    Removed file: pkg/common/logger/logger.go
    Completely migrate to klog.
    Example usage:
    	go run main.go -v=5 -log_file=/logs/vodascheduler.log -alsologtostderr -logtostderr=false

--- pkg/main.go
+++ pkg/main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"github.com/heyfey/vodascheduler/config"
-	"github.com/heyfey/vodascheduler/pkg/common/logger"
 	"github.com/heyfey/vodascheduler/pkg/service"
 	"k8s.io/klog/v2"
 )
@@ -14,20 +13,16 @@ import (
 func main() {
 	fmt.Printf("%s (v%s)\n", config.Msg, config.Version)
 
-	// flag definition should placed before logger.InitLogger()
 	/* flags */
 	kubeconfigPtr := flag.String("kubeconfig", "", "absolute path to the kubeconfig file (required if not running within pod)")
 	/* flags end */
 
-	logger.InitLogger()
-	defer klog.Flush()
-
-	klog.InfoS(config.Msg, "version", config.Version)
-	klog.InfoS("Starting service")
-
+	klog.InitFlags(nil)
 	if !flag.Parsed() {
 		flag.Parse()
 	}
+	klog.InfoS(config.Msg, "version", config.Version)
+	klog.InfoS("Starting service")
 	klog.InfoS("Listing flags", "kubeconfig", *kubeconfigPtr)
 
 	service := service.NewService(*kubeconfigPtr)
